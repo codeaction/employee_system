887a9814a0f222c127c62cc94f3a74319c6c00e2
Maxwell <maxwell0811@126.com>
2022-12-12 23:35:46 +0800

    删除多余的内容

--- src/com/stedu/controller/AdminController.java
+++ src/com/stedu/controller/AdminController.java
@@ -15,7 +15,7 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
-@WebServlet(name = "AdminController", value = {"/login", "/logout", "/verificationCode", "/aaa"})
+@WebServlet(name = "AdminController", value = {"/login", "/logout", "/verificationCode"})
 public class AdminController extends HttpServlet {
     private AdminService adminService = new AdminServiceImpl();
 
